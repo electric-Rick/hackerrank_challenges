0c4c4cb470a19e24d434d7ae83a7b302bc00a2f6
e-Rick <erick.augusto.ebs@gmail.com>
2021-10-25 23:31:29 -0300

    Adição de comentários em inglês
    Os comentários estavam sem uma descrição clara dos acontecimentos dentro do código.
    Resolvi ajustar e demonstrar cada passo que é feito dentro do código... E como podem ver, o Python é uma mãe.

--- Solutions in Python/FizzBuzzSolution.py
+++ Solutions in Python/FizzBuzzSolution.py
@@ -1,15 +1,15 @@
 def fizzBuzz(n):
     # Write your code here
     for x in range(1, n+1, 1):
-        if (x%5) == 0 and (x%3) == 0:
-            print('FizzBuzz')
-        elif (x%5) == 0:
-            print('Buzz')
+        if (x%5) == 0 and (x%3) == 0: # if both conditions is real
+            print('FizzBuzz') # print FizzBuzz
+        elif (x%5) == 0: # if this condition is real
+            print('Buzz') # print Buzz
             pass
-        elif (x%3) == 0:
-            print('Fizz')
+        elif (x%3) == 0: # if this condition is real
+            print('Fizz') # print Fizz
             pass
-        else:
-            print(x)
-            pass       #print the number 
+        else: # if none conditions is real
+            print(x) #print the number only
+            pass       
 pass
