1d25f11bd0ebeb3c466e65642a42993027c85451
e-Rick <erick.augusto.ebs@gmail.com>
2021-10-25 23:33:34 -0300

    Update FizzBuzzSolution.py

--- Solutions in Python/FizzBuzzSolution.py
+++ Solutions in Python/FizzBuzzSolution.py
@@ -1,6 +1,5 @@
-def fizzBuzz(n):
-    # Write your code here
-    for x in range(1, n+1, 1):
+def fizzBuzz(n): # Define the function and yours parameter
+    for x in range(1, n+1, 1): # For each number in the range n + 1...
         if (x%5) == 0 and (x%3) == 0: # if both conditions is real
             print('FizzBuzz') # print FizzBuzz
         elif (x%5) == 0: # if this condition is real
